a76c4abb1a489b26bc956dced7e9573b40fa64ca
Dotwood Media <info@dotwood.media>
2022-06-17 12:10:43 +0200

    1e ontwerp

--- summamove-mobile/App.js
+++ summamove-mobile/App.js
@@ -1,7 +1,50 @@
-import Home from './screens/Home';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import HomeScreen from './screens/Home';
+
+const Tab = createMaterialBottomTabNavigator();
 
 export default function App() {
   return (
-    <Home />
+    <NavigationContainer>
+      <Tab.Navigator
+        barStyle={{ backgroundColor: '#ffffff' }}
+        activeColor="#d60096"
+        inactiveColor="#25126e">
+        <Tab.Screen name="Home"
+          options={{
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="home" color={color} size={26} />
+            ),
+          }}
+          component={HomeScreen} />
+
+        <Tab.Screen name="Oefeningen"
+          options={{
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="ballot" color={color} size={26} />
+            ),
+          }}
+          component={HomeScreen} />
+
+        <Tab.Screen name="Prestaties"
+          options={{
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="star" color={color} size={26} />
+            ),
+          }}
+          component={HomeScreen} />
+
+        <Tab.Screen name="Over ons"
+          options={{
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="book-account" color={color} size={26} />
+            ),
+          }}
+          component={HomeScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
--- summamove-mobile/screens/Home.js
+++ summamove-mobile/screens/Home.js
@@ -1,21 +1,37 @@
-import React from 'react'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { Text, View, SafeAreaView } from 'react-native';
+
+import tw from 'tailwind-react-native-classnames';
 
 export default function Home() {
     return (
-        <View style={styles.container}>
-            <Text>Open up App.js to start working on your app!</Text>
+        <SafeAreaView style={tw.style('bg-gray-100 h-full px-5 py-14')}>
+            <Text style={tw.style('font-black text-3xl')}>Hallo Pascal 👋</Text>
+            <Text style={tw.style('font-bold text-lg')}>Laten we je activiteit checken</Text>
             <StatusBar style="auto" />
-        </View>
+
+            <View>
+                <View style={tw.style('flex-row mt-5')}>
+                    <View style={tw.style('flex-1 bg-gray-200 rounded-3xl p-5 mr-4 shadow-lg')}>
+                        <Text style={tw.style('font-bold text-lg text-center')}>💪 Afgerond</Text>
+                        <Text style={tw.style('text-4xl font-bold text-center mt-3 mb-2')}>10</Text>
+                        <Text style={tw.style('text-center opacity-60 text-lg')}>Afgeronde oefeningen</Text>
+                    </View>
+
+                    <View style={tw.style('flex-1 bg-gray-200 rounded-3xl p-5 mr-4 shadow-lg')}>
+                        <Text style={tw.style('font-bold text-lg text-center')}>💪 Afgerond</Text>
+                        <Text style={tw.style('text-4xl font-bold text-center mt-3 mb-2')}>10</Text>
+                        <Text style={tw.style('text-center opacity-60 text-lg')}>Afgeronde oefeningen</Text>
+                    </View>
+                </View>
+
+                <View style={tw.style('bg-gray-200 rounded-3xl p-5 mr-4 shadow-lg mt-5')}>
+                    <Text style={tw.style('font-bold text-lg text-center')}>💪 Afgerond</Text>
+                    <Text style={tw.style('text-4xl font-bold text-center mt-3 mb-2')}>10</Text>
+                    <Text style={tw.style('text-center opacity-60 text-lg')}>Afgeronde oefeningen</Text>
+                </View>
+            </View>
+        </SafeAreaView>
     )
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
